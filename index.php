31cc8be008dcf0b4e56906308145163f9858745b
Shubhamsingh369 <95709140+Shubhamsingh369@users.noreply.github.com>
2023-10-05 16:35:33 +0530

    adding index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    // session_start();
+    // require ('sales.php');
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+  <?php include('components/ythead.html'); ?>
+<script src="./js/make_sticky.js" type="text/javascript"></script>
+</head>
+<body>
+<!-- Sidebar --> 
+<?php include('components/ytheader.html'); ?> 
+   <div class="container">
+   
+      <img  class="sticky" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkrhr9B7kEHayxs6ZFqjWXIrqC_oueOspiWA&usqp=CAU" alt="Snow" style="width:84%; margin-left: 16%;">
+      <!-- <div class="top-right"><img src="https://www.younitedtechnologies.com/wp-content/uploads/2021/12/logo-1.jpg" alt="show"></div> -->
+
+   </div>
+<?php include('components/ytfooter.html'); ?>
+</body>
+</html>
+
+
+<!-- https://www.fueloyal.com/wp-content/uploads/2017/12/Objectives-and-Goals-of-a-Good-Logistics-Company-6-650x617.jpg -->
